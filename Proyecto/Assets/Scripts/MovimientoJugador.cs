78659d1194f40c306053721cf46db8fd808f8488
Skyth85 <154547420+Skyth85@users.noreply.github.com>
2024-04-10 17:57:54 +0200

    Update MovimientoJugador.cs

--- Proyecto/Assets/Scripts/MovimientoJugador.cs
+++ Proyecto/Assets/Scripts/MovimientoJugador.cs
@@ -26,29 +26,31 @@ public class MovimientoJugador : MonoBehaviour
     }
     private void InputJugador()
     {
-        Vector2 aux = Vector2.zero; // Declaramos el Vector2 para el movimiento y lo identificamos como aux la variable
+        Vector2 aux = Vector2.zero; // Declaramos el Vector2 para el movimiento y lo identificamos como aux la variable y ponemos que es 0 para que en caso de no pulsar nada se quede quieto
         if (Input.GetKey(moveUp)) // Comprobamos que se está pulsando la tecla W
         {
             // Nos desplazamos (sumamos movimiento) hacia arriba (eje y = 1), multiplicamos por deltatime
-            // para que el movimiento no dependa del framerate.
+            // para que el movimiento no dependa del framerate ya que lo gestiona el motor de fisicas.
             aux.y = 1;
            
         }
         if (Input.GetKey(moveDown))
         {
+            // Nos desplazamos (sumamos movimiento) hacia abajo (eje y = -1), multiplicamos por deltatime
             aux.y = -1;
         }
         if (Input.GetKey(moveLeft))
         {
+            // Nos desplazamos (sumamos movimiento) hacia la izquierda (eje x = -1), multiplicamos por deltatime
             aux.x = -1;
 
         }
         if (Input.GetKey(moveRight))
         {
+            // Nos desplazamos (sumamos movimiento) hacia la derecha (eje x = 1), multiplicamos por deltatime
             aux.x = 1;
         }
-        
-        rb.velocity = aux.normalized * speed;
+        rb.velocity = aux.normalized * speed; // Aqui lo normalizamos y lo multiplicamos por speed para que no vaya mas rapido en diagonal
     }
 
 }
