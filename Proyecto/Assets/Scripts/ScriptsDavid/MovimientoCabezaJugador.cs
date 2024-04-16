c45b3d7cb9faf8de3711436677bbbca1b7b209ac
Skyth85 <154547420+Skyth85@users.noreply.github.com>
2024-04-16 17:56:58 +0200

    Avance Bala
    Avance de la bala, y  añadido de una escena de pruebas

--- Proyecto/Assets/Scripts/ScriptsDavid/MovimientoCabezaJugador.cs
+++ Proyecto/Assets/Scripts/ScriptsDavid/MovimientoCabezaJugador.cs
@@ -7,6 +7,8 @@ public class MovimientoCabezaJugador : MonoBehaviour
 {
     public Direction dirCabeza;
     public Animator pAnimator;
+    public float speedBullet = 3f;
+    public GameObject balaprefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,22 +25,41 @@ public class MovimientoCabezaJugador : MonoBehaviour
         if(Input.GetKey(KeyCode.UpArrow))
         {
             dirCabeza = Direction.UP;
-           // pAnimator.Play("Mirar_Arriba");
+            // pAnimator.Play("Mirar_Arriba");
+            Shoot();
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            dirCabeza = Direction.LEFT;
-          //  pAnimator.Play("Mirar_Derecha");
+            dirCabeza = Direction.RIGHT;
+            //  pAnimator.Play("Mirar_Derecha");
+            Shoot();
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             dirCabeza = Direction.LEFT;
-           // pAnimator.Play("Mirar_Izquierda");
+            // pAnimator.Play("Mirar_Izquierda");
+            Shoot();
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             dirCabeza = Direction.DOWN;
-           // pAnimator.Play("Mirar_Abajo");
+            // pAnimator.Play("Mirar_Abajo");
+            Shoot();
         }
+    }
+           // transform.position += new Vector3(0, speedBullet, 0) * Time.deltaTime;
+           //transform.position += new Vector3(speedBullet, 0, 0) * Time.deltaTime;
+           // transform.position += new Vector3(-speedBullet, 0, 0) * Time.deltaTime;
+           // transform.position += new Vector3(0, -speedBullet, 0) * Time.deltaTime;
+    public void Shoot()
+    {
+        // TODO cambiar posicion según dirección de la cabeza
+        GameObject bala = Instantiate(balaprefab, transform.position, Quaternion.identity);
+        // Obtener componente BalaJugador del objeto
+        // A esa BalaJugador le asignamos la dirección (la de la cabeza)
+
+        // Desactivar el disparo (p.ej. booleano canShoot)
+        // Hacer un invoke para volver a activar canShoot en un tiempo p.ej. delayDisparo)
+
     }
 }
