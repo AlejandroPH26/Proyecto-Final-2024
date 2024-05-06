037b44cde2c832a7ca44c39e9e4c53639245723c
Alejandro <alejandroph2699@gmail.com>
2024-05-06 17:41:00 +0200

    Items final de sala

--- Proyecto/Assets/Scripts/ScriptsPruebasAlejandro/SalasManager.cs
+++ Proyecto/Assets/Scripts/ScriptsPruebasAlejandro/SalasManager.cs
@@ -7,6 +7,8 @@ public class SalasManager : MonoBehaviour
 
     public string teleportTargetTag = "TeleportTarget";
     public string currentRoomTag = "SalaActual";
+    public GameObject bombaPrefab;
+    public GameObject botiquinPrefab;
 
     public List<GameObject> puertas;
     private List<GameObject> enemigosEnSala = new List<GameObject>();
@@ -64,6 +66,7 @@ public class SalasManager : MonoBehaviour
                 if (comportamientoEnemigo != null)
                 {
                     comportamientoEnemigo.enabled = false;
+                    enemigo.GetComponent<SpriteRenderer>().enabled = false; // Desactivar el SpriteRenderer
                     CongelarEnemigos(enemigo);
                 }
             }
@@ -83,6 +86,7 @@ public class SalasManager : MonoBehaviour
             if (comportamientoEnemigo != null)
             {
                 comportamientoEnemigo.enabled = true;
+                enemigo.GetComponent<SpriteRenderer>().enabled = true; // Activar el SpriteRenderer
             }
         }
     }
@@ -141,6 +145,31 @@ public class SalasManager : MonoBehaviour
             {
                 DesactivarEnemigos();
                 ActivarCollidersPuertas();
+
+                // Generar un número aleatorio entre 0 y 1
+                float randomValue = Random.Range(0f, 1f);
+
+                // Si el número aleatorio es mayor que 0.5, se instancia un objeto
+                if (randomValue > 0.5f)
+                {
+                    // Generar otro número aleatorio para determinar si se instancia una bomba o un botiquín
+                    float itemTypeChance = Random.Range(0f, 1f);
+
+                    if (itemTypeChance > 0.5f)
+                    {
+                        // Instanciar una bomba
+                        Instantiate(bombaPrefab, transform.position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        // Instanciar un botiquín
+                        Instantiate(botiquinPrefab, transform.position, Quaternion.identity);
+                    }
+                }
+                else
+                {
+                    Debug.Log("No se ha consseguido nada");
+                }
             }
         }
 
